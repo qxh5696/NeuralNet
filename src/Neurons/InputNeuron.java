1400d6fbf2a633c76a42c95fbc3468b39d2e50c1
Qadir Haqq <Haqq.Q173@gmail.com>
2016-04-02 00:16:38 -0400

    Switching over devices

--- src/Neurons/InputNeuron.java
+++ src/Neurons/InputNeuron.java
@@ -3,6 +3,11 @@ package Neurons;
 /**
  * Created by qadirhaqq on 4/1/16.
  */
-public class InputNeuron {
+public class InputNeuron implements Neuron{
 
+
+    @Override
+    public float compute() {
+        return 0;
+    }
 }

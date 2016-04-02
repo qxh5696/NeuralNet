6b75c4e27df33b01bd406cffc6caea58b9d696d3
Josh Miller <jxm7374@rit.edu>
2016-04-02 03:37:44 -0400

    General Class elements for distribution

--- src/data_processing/Distribution.java
+++ src/data_processing/Distribution.java
@@ -1,7 +1,50 @@
 package data_processing;
 
+import java.util.List;
+
 /**
  * Created by josh on 4/2/16.
  */
 public abstract class Distribution {
+    private List<DistributionIndex> indices;
+    private double highBound, lowBound;
+    private int numOfIndices;
+
+    public Distribution(double highBound, double lowBound, int numOfIndices) {
+        this.highBound = highBound;
+        this.lowBound = lowBound;
+        this.numOfIndices = numOfIndices;
+    }
+
+    public List<DistributionIndex> getIndices() {
+        return indices;
+    }
+
+    public void setIndices(List<DistributionIndex> indices) {
+        this.indices = indices;
+    }
+
+    public double getHighBound() {
+        return highBound;
+    }
+
+    public void setHighBound(double highBound) {
+        this.highBound = highBound;
+    }
+
+    public double getLowBound() {
+        return lowBound;
+    }
+
+    public void setLowBound(double lowBound) {
+        this.lowBound = lowBound;
+    }
+
+    public int getNumOfIndices() {
+        return numOfIndices;
+    }
+
+    public void setNumOfIndices(int numOfIndices) {
+        this.numOfIndices = numOfIndices;
+    }
 }
--- src/data_processing/DistributionIndex.java
+++ src/data_processing/DistributionIndex.java
@@ -4,4 +4,26 @@ package data_processing;
  * Created by josh on 4/2/16.
  */
 public class DistributionIndex {
+    private double high,low;
+
+    public DistributionIndex(double high, double low) {
+        this.high = high;
+        this.low = low;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
 }

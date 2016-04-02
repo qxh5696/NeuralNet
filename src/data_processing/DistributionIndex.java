5201b9209fb02bf54c3cf6034d36922cf57acf46
Josh Miller <jxm7374@rit.edu>
2016-04-02 03:31:57 -0400

    Added Distribution framework elements

---
+++ src/data_processing/DistributionIndex.java
@@ -0,0 +1,7 @@
+package data_processing;
+
+/**
+ * Created by josh on 4/2/16.
+ */
+public class DistributionIndex {
+}
--- src/data_processing/Parser.java
+++ src/data_processing/Parser.java
@@ -1,8 +1,42 @@
 package data_processing;
 
+import java.io.*;
+
 /**
  * Created by josh on 4/2/16.
  */
 public class Parser {
 
+    public static double[] parseLine(String s){
+        String[] items = s.replace(" ","").split(",");
+        if(items[1].equals("/muse/eeg")){
+            double[] list = {Double.parseDouble(items[2]),Double.parseDouble(items[3]),Double.parseDouble(items[4]),Double.parseDouble(items[5])};
+            return list;
+        }
+        return null;
+    }
+
+    public static void parse(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("recording.csv")));
+            String s;
+            while ( (s=bufferedReader.readLine()) != null){
+                double[] list = parseLine(s);
+                if (list!= null){
+                    for (double d: list){
+                        System.out.printf("%15.5f ", d);
+                    }
+                    System.out.printf("\n");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        parse();
+    }
 }
